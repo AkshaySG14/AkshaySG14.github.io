2a9544805c39f8022531053ec8d50fa70ec02bdb
akshay <akshaysg@umich.edu>
2018-09-04 14:47:53 -0400

    Updates

--- src/js/actions/index.js
+++
@@ -1,7 +0,0 @@
-import {CHANGE_PAGE, CHANGE_SIDEBAR_ACTIVE, CHANGE_SOURCE_CODE_COMPONENT} from "../constants/ActionTypes"
-
-export const changePage = (newPage) => ({ type: CHANGE_PAGE, payload: {newPage} });
-
-export const changeSidebarActive = (newComponent) => ({ type: CHANGE_SIDEBAR_ACTIVE, payload: {newComponent} });
-
-export const changeSourceCodeComponent = (sourceCodeNumber) => ({ type: CHANGE_SOURCE_CODE_COMPONENT, payload: {sourceCodeNumber} });
--- src/js/constants/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const CHANGE_PAGE = "change_page";
-export const CHANGE_SIDEBAR_ACTIVE = "change_sidebar_active";
-export const CHANGE_SOURCE_CODE_COMPONENT = "change_source_code_component";
